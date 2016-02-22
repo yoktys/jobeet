64ce0ea799c19a9ccad2926f6ce0775f5a4eb943
yokoyama <yokoyama@vcube.co.jp>
2016-02-22 15:51:23 +0900

    Implemented Day 7

--- apps/frontend/modules/category/actions/actions.class.php
+++ apps/frontend/modules/category/actions/actions.class.php
@@ -18,5 +18,14 @@ class categoryActions extends sfActions
   public function executeShow(sfWebRequest $request)
   {
       $this->category = $this->getRoute()->getObject();
+
+      // pager
+      $this->pager = new sfPropelPager(
+          'JobeetJob',
+          sfConfig::get('app_max_jobs_on_category')
+      );
+      $this->pager->setCriteria($this->category->getActiveJobsCriteria());
+      $this->pager->setPage($request->getParameter('page', 1));
+      $this->pager->init();
   }
 }
--- lib/model/JobeetCategory.php
+++ lib/model/JobeetCategory.php
@@ -23,18 +23,16 @@ class JobeetCategory extends BaseJobeetCategory {
     }
 
     public function getActiveJobs($max = 10) {
-        $criteria = new Criteria();
-        $criteria->add(JobeetJobPeer::CATEGORY_ID, $this->getId());
+        $criteria = $this->getActiveJobsCriteria();
         $criteria->setLimit($max);
 
-        return JobeetJobPeer::getActiveJobs($criteria);
+        return JobeetJobPeer::doSelect($criteria);
     }
 
     public function countActiveJobs() {
-        $criteria = new Criteria();
-        $criteria->add(JobeetJobPeer::CATEGORY_ID, $this->getId());
+        $criteria = $this->getActiveJobsCriteria();
 
-        return JobeetJobPeer::countActiveJobs($criteria);
+        return JobeetJobPeer::doCount($criteria);
     }
 
     public function getSlug() {
@@ -44,5 +42,12 @@ class JobeetCategory extends BaseJobeetCategory {
     public function setName($name) {
         parent::setName($name);
         $this->setSlug(Jobeet::slugify($name));
+    }
+
+    public function getActiveJobsCriteria() {
+        $criteria = new Criteria();
+        $criteria->add(JobeetJobPeer::CATEGORY_ID, $this->getId());
+
+        return JobeetJobPeer::addActiveJobsCriteria($criteria);
     }
 } // JobeetCategory
